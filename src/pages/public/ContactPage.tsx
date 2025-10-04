956df39be32ef0312fc6b3e0bf3a5e592624963e
yi-dev-art <yipenebayala0@gmail.com>
2025-10-04 17:39:49 +0200

    Updated ContactPage.tsx

--- src/pages/public/ContactPage.tsx
+++ src/pages/public/ContactPage.tsx
@@ -66,7 +66,7 @@ export const ContactPage = () => {
         </div>
       </header>
 
-      <div className="w-full px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
